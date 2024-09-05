9c732cdf5fac4853a67d2fc0281625826bcdfea7
Jackson Leste <jacksongoncalves.leste@gmail.com>
2024-09-05 20:47:29 -0300

    Feat: adicionando o header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', redirectTo: 'home', pathMatch: 'full'  },
+  {path: '**', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent}
 ];
